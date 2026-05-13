920dfdee631ee510c8540d0d6dfcec021f46d0ef
selimdawa <selimdawa@gmail.com>
2026-05-14 02:41:49 +0300

    VER 1.0

---
+++ app/src/main/java/com/littleapp/candycrushgame/Unit/CLASS.kt
@@ -0,0 +1,8 @@
+package com.littleapp.candycrushgame.Unit
+
+import com.littleapp.candycrushgame.Activity.*
+
+object CLASS {
+    val MAIN: Class<*> = MainActivity::class.java
+    val SPLASH: Class<*> = SplashActivity::class.java
+}
---
+++ app/src/main/java/com/littleapp/candycrushgame/Unit/DATA.kt
@@ -0,0 +1,12 @@
+package com.littleapp.candycrushgame.Unit
+
+object DATA {
+
+    //Other
+    const val EMPTY = ""
+    const val SPACE = " "
+    const val Unknown = "Unknown"
+
+
+
+}
