d190d4190be9dc91c5e71a2cf410e8bbab031ac0
selimdawa <selimdawa@gmail.com>
2026-08-29 06:58:44 +0300

    Edge to Edge - Add

--- app/src/main/java/com/littleapp/candycrushgame/Activity/MainActivity.kt
+++ app/src/main/java/com/littleapp/candycrushgame/Activity/MainActivity.kt
@@ -9,7 +9,10 @@ import android.os.Looper
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.annotation.RequiresApi
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.littleapp.candycrushgame.R
 import com.littleapp.candycrushgame.Unit.OnSwipeListener
 import com.littleapp.candycrushgame.Unit.THEME
@@ -41,9 +44,16 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         THEME.setThemeOfApp(context)
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         binding.toolbar.nameSpace.text = getString(R.string.candy_crush_game)
 
